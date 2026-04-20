970e00ab4e0cc9202fa2481d3af540a2ec0e05c1
YehorMartynenko <yehor.martynenko.27@gmail.com>
2026-04-20 22:35:23 +0200

    Refactor webpack.config to use webpack-merge

--- webpack.common.js
+++ webpack.common.js
@@ -1,23 +1,18 @@
-import path from "node:path";
-import HtmlWebpackPlugin from "html-webpack-plugin";
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default {
-    mode: "development",
-    entry: "./src/index.js",
-    output: {
-        filename: "main.js",
-        path: path.resolve(import.meta.dirname, "dist"),
-        clean: true,
-    },
+    entry: './src/index.js',
     plugins: [
         new HtmlWebpackPlugin({
-            template: "./src/template.html",
+            title: 'Production',
+            template: './src/template.html',
         }),
     ],
-    devtool: "eval-source-map",
-    devServer: {
-        watchFiles: ["./src/template.html"],
-    },
     module: {
         rules: [
             {
@@ -33,5 +28,10 @@ export default {
                 type: "asset/resource",
             },
         ],
+    },
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
     },
 };
